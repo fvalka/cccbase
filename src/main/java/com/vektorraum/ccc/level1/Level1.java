93233e7284ea05630cc103b0febe72a549e59c63
Fabian Valka <fvalka@vektorraum.com>
2019-03-20 17:47:17 +0100

    add harvester1

--- src/main/java/com/vektorraum/ccc/level1/Level1.java
+++ src/main/java/com/vektorraum/ccc/level1/Level1.java
@@ -10,7 +10,34 @@ import java.util.List;
 public class Level1 extends Level {
     @Override
     public String apply(List<String> strings) {
-        return strings.get(0);
+
+        String[] fieldDef = strings.get(0).split(" ");
+
+        int rows = Integer.parseInt(fieldDef[0]);
+        int cols = Integer.parseInt(fieldDef[1]);
+        int[][] field = new int[rows][cols];
+
+        for(int row= 0; row < rows; row++){
+            for(int col = 0; col < cols; col++){
+                field[row][col] = row * cols+col+1;
+            }
+        }
+
+        String res = "";
+
+        for(int currRow = 0; currRow < rows; currRow++){
+            if(currRow % 2 == 0){
+                for(int i = 0; i < cols; i++){
+                    res += field[currRow][i] +" ";
+                }
+            }else{
+                for (int i= cols-1; i >= 0; i--){
+                    res += field[currRow][i] + " ";
+                }
+            }
+        }
+
+        return res;
     }
 
     public static void main(String[] args) throws Exception {
